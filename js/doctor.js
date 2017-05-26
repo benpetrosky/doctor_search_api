a393220d134e4c2776db186e7015b4a4d3fc0794
Ben Petrosky <benpetrosky@hotmail.com>
2017-05-26 12:16:45 -0700

    start adding sass, sets limit to length of data object

--- js/doctor.js
+++ js/doctor.js
@@ -12,12 +12,14 @@ Doctor.prototype.getDoctor = function(symptom, doctorInfo){
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+ symptom + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey).then(function(response){
 
 
-    for (var i=0; i < 20; i++) {
-    var first_name = response.data[i].profile.first_name;
-    var last_name = response.data[i].profile.last_name;
-    newDoctor = new Doctor(first_name, last_name);
+    limit = response.data.length;
+      for (var i=0; i < limit ; i++) {
 
-    doctors.push(newDoctor);
+      var first_name = response.data[i].profile.first_name;
+      var last_name = response.data[i].profile.last_name;
+      newDoctor = new Doctor(first_name, last_name);
+
+      doctors.push(newDoctor);
 
 
 
@@ -25,6 +27,7 @@ Doctor.prototype.getDoctor = function(symptom, doctorInfo){
   }
   doctorInfo(doctors);
 });
+
 };
 
 
